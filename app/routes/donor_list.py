7482a22118684ad700eaa8433234e3be272e675e
Averkin Mathew <meazerg@gmail.com>
2024-05-19 22:59:49 +0300

    app list site

--- app/routes/donor_list.py
+++ app/routes/donor_list.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from ..extensions import db
+from ..models.donor import Donor
 
 donor_list = Blueprint('donor_list', __name__)
 
@@ -6,3 +8,11 @@ donor_list = Blueprint('donor_list', __name__)
 @donor_list.route('/donor/list')
 def index():
     return render_template('main/index.html')
+
+
+# @donor_list.route('/donor/list')
+# def create():
+#     donor_list = Donor()
+#     db.session.add(donor_list)
+#     db.session.commit()
+#     return 'Donor Create'
--- app/routes/login.py
+++ app/routes/login.py
@@ -6,3 +6,8 @@ login = Blueprint('login', __name__)
 @login.route('/login')
 def index():
     return render_template('main/login.html')
+
+
+# @login.route('/login/examination', method=['POST'])
+# def examination():
+#     return render_template('main/login.html')
